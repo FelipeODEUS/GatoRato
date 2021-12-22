e9d79fa4bcb2271e20ae77a7467c615e35dd9fc3
FelipeODEUS <92820937+FelipeODEUS@users.noreply.github.com>
2021-12-22 10:58:18 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,13 +1,17 @@
 var bgImg;
 var cat;
+var mouse;
+var catImg1, catImg2, catImg3;
+var mouseImg1, mouseImg2, mouseImg3;
 
 function preload() {
-    //load the images here
     bgImg = loadImage("images/garden.png");
-    catImg1 = loadImage("images/cat1.png");
-    mouseImg1 = loadImage("images/mouse1.png");
-    catImg2 = loadImage("images/cat2.png, images/cat3.png");
-    mouseImg2 = loadImage("images/mouse2.png, images/mouse3.png");
+    catImg1 = loadAnimation("images/tom1.png");
+    catImg2 = loadAnimation("images/tom2.png", "images/tom3.png");
+    catImg3 = loadAnimation("images/tom4.png");
+    mouseImg1 = loadAnimation("images/jerry1.png");
+    mouseImg2 = loadAnimation("images/jerry2.png", "images/jerry3.png");
+    mouseImg3 = loadAnimation("images/jerry4.png");
 }
 
 function setup(){
@@ -17,22 +21,30 @@ function setup(){
     cat.addAnimation("gatoSentado", catImg1);
     cat.scale = 0.2;
 
+    mouse = createSprite(200,600);
+    mouse.addAnimation("ratoSentado", mouseImg1);
+    mouse.scale = 0.15;
+
 }
 
 function draw() {
 
-    background("images/garden.png");
+    background(bgImg);
     //Write condition here to evalute if tom and jerry collide
     if(cat.x - mouse.x < (cat.width - mouse.width)/2){
         cat.velocityX = 0;
         cat.addAnimation("ultimaImagemGato", catImg3);
-        cat.changeAnimation("ultimaImagemGato");
         cat.x = 300;
         cat.scale = 0.2;
+        cat.changeAnimation("ultimaImagemGato");
+        
+        mouse.addAnimation("ultimaImagemRato", mouseImg3);
+        mouse.scale = 0.15;
+        mouse.changeAnimation("ultimaImagemRato");
     }
 
-
     drawSprites();
+
 }
 
 
@@ -40,16 +52,13 @@ function keyPressed(){
 
   //For moving and changing animation write code here
   if(keyCode === LEFT_ARROW){
-      mouse.addAnimation("ratoProvocando", mouseImg2);
-      mouse.changeAnimation("ratoProvocando");
-      mouse.frameDelay = 25;
-  }
-
-  
-
-  if(cat.x - mouse.x < (cat.width - mouse.width)/2){
-      cat.addAnimation("gatoCorrendo", catImg2);
-      cat.changeAnimation("gatoCorrendo");
       cat.velocityX = -5;
+      cat.addAnimation("gatoCorrendo", catImg2);
+      cat.changeAnimation("gatoCorrendo"); 
+    
+      mouse.addAnimation("ratoProvocando", mouseImg2);
+      mouse.frameDelay = 25;
+      mouse.changeAnimation("ratoProvocando");
+      
   }
 }
